7e24ab78c802fdace8e40cb7d9e56bb211d06fda
Greniel Galinato <greniel.galinato@gmail.com>
2023-11-17 22:25:58 +0800

    fem_intro_component_with_signup_form: setup main contents and components of the react app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+        <footer>
+            <p className="attribution">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
+            Coded by <a href="#">Your Name Here</a>.
+            </p>
+        </footer>
+    )
+}
+
+export default Footer;
