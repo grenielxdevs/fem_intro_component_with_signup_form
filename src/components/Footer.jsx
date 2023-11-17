633db51191898cdfdb136a9ba9609894588c1818
Greniel Galinato <greniel.galinato@gmail.com>
2023-11-17 22:53:46 +0800

    fem_intro_component_with_signup_form: alignment and footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <footer>
+        <footer id="footer">
             <p className="attribution">
             Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
             Coded by <a href="#">Your Name Here</a>.
