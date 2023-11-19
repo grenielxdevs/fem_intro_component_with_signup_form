a838d34a188e887d49c1ba644f3d569889155548
Greniel Galinato <greniel.galinato@gmail.com>
2023-11-19 21:39:08 +0800

    fem_intro_component_with_signup_form: adding footer styles and contents

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
         <footer id="footer">
             <p className="attribution">
             Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
-            Coded by <a href="#">Your Name Here</a>.
+            Coded by <a href="https://github.com/grenielxdevs">Greniel Galinato | grenielxdevs</a>.
             </p>
         </footer>
     )
